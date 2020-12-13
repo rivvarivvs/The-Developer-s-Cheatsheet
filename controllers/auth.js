8353d008ddf6c70c8a982bba35273d9f23339652
rivva <rivvarivvs@gmail.com>
2020-12-13 02:31:00 +0000

    fixed some imports and configured the sending email options for when signing up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,20 @@
-const express = require("express");
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+
+const User = require("../models/User");
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: process.env.sendGrid_key
+  }
+}));
 
 exports.getLogin((req, res, next) => {
   res.render("/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
+    errorMessage: req.flash("error"),
   });
 });
 
@@ -13,36 +22,36 @@ exports.getSignup = (req, res, next) => {
   res.render("register", {
     path: "/register",
     pageTitle: "Signup",
-    isAuthenticated: false,
   });
 };
 
 exports.postLogin((req, res, next) => {
-  const { email, password } = req.body
+  const { email, password } = req.body;
   User.findOne({ email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email.')
-        return res.redirect('/login')
+        req.flash("error", "Invalid email.");
+        return res.redirect("/login");
       }
-      bcrypt.compare(password, user.password)
-      .then(doMatch => {
-        if (doMatch) {
-          req.session.isLoggedIn = true
-          req.session.user = user
-          return req.session.save(err => {
-            console.log(err)
-            res.redirect('/')
-          })
-        }
-        res.redirect('/login')
-      })
-      .catch(err => {
-        console.log(err)
-      })
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              console.log(err);
+              res.redirect("/");
+            });
+          }
+          res.redirect("/login");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     })
-    .catch(err => console.log(err))
-})
+    .catch((err) => console.log(err));
+});
 
 exports.postLogout((req, req, next) => {
   req.session.destroy((err) => {
@@ -63,18 +72,28 @@ exports.postSignup((req, res) => {
       if (user) {
         return res.status(400).json({ msg: "User already exists" });
       }
-      return bcrypt
-      .hash(password, 12)
-      .then(hashedPassword => {
+      return bcrypt.hash(password, 12).then((hashedPassword) => {
         const newUser = new User({
           name: name,
           email: email,
           password: hashedPassword,
         });
         return newUser.save();
-      });
+      })
+      .then(result => {
+        res.redirect('/login')
+        return transporter.sendMail({
+          to: email,
+          from: 'admin@complete.com',
+          subject: 'You\'re registered!',
+          html: '<h1>You signed up!</h1>'
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
     })
-    .catch(err => {
-      console.log(err)
-    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
